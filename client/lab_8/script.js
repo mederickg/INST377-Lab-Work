500209a8123f46772aa3148aaa956931be750fac
Médérick Charles Fernand Grivel <grivel.mederick@gmail.com>
2022-11-15 17:24:41 -0500

    done lab 8

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -12,18 +12,18 @@
 */
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
 }
 function injectHTML(list) {
-  console.log("fired injectHTML");
-  const target = document.querySelector("#resturaunt_list");
-  target.innerHTML = "";
-  const listEl = document.createElement("ol");
+  console.log('fired injectHTML');
+  const target = document.querySelector('#resturaunt_list');
+  target.innerHTML = '';
+  const listEl = document.createElement('ol');
   target.appendChild(listEl);
   list.forEach((item) => {
-    const el = document.createElement("li");
+    const el = document.createElement('li');
     el.innerText = item.name;
     listEl.appendChild(el);
   });
@@ -35,7 +35,7 @@ function injectHTML(list) {
         the usual ones are element.innerText and element.innerHTML
         Here's an article on the differences if you want to know more:
         https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
-  
+
       ## What to do in this function
         - Accept a list of restaurant objects
         - using a .forEach method, inject a list element into your index.html for every element in the list
@@ -44,7 +44,7 @@ function injectHTML(list) {
 }
 
 function processRestaurants(list) {
-  console.log("fired restaurants list");
+  console.log('fired restaurants list');
   const range = [...Array(15).keys()];
   const newArray = range.map((item) => {
     const index = getRandomIntInclusive(0, list.length);
@@ -58,12 +58,12 @@ function processRestaurants(list) {
           then select 15 random records
           and return an object containing only the restaurant's name, category, and geocoded location
           So we can inject them using the HTML injection function
-  
+
           You can find the column names by carefully looking at your single returned record
           https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
-  
+
         ## What to do in this function:
-  
+
         - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
         - using a .map function on that range,
         - Make a list of 15 random restaurants from your list of 100 from your data request
@@ -82,26 +82,51 @@ function filterList(list, filterInputValue) {
   });
 }
 
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
+function markerPlace(array, map) {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+  array.forEach((item, index) => {
+    const {coordinates} = item.geocoded_column_1;
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 12);
+    }
+  });
+}
+
 async function mainEvent() {
   /*
         ## Main Event
           Separating your main programming from your side functions will help you organize your thoughts
           When you're not working in a heavily-commented "learning" file, this also is more legible
           If you separate your work, when one piece is complete, you can save it and trust it
-      */
-
+  */
+  const pageMap = initMap();
   // the async keyword means we can make API requests
-  const form = document.querySelector(".main_form"); // get your main form so you can do JS with it
-  const submit = document.querySelector("#get-resto"); // get a reference to your submit button
-  const loadAnimation = document.querySelector(".lds-ellipsis"); // get a referance to our loading animation
-  submit.style.display = "none"; // let your submit button disappear
+  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
+  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
+  const loadAnimation = document.querySelector('.lds-ellipsis'); // get a referance to our loading animation
+  submit.style.display = 'none'; // let your submit button disappear
 
   /*
         Let's get some data from the API - it will take a second or two to load
         This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
         It's at about line 27 - go have a look and see what we're retrieving and sending back.
-       */
-  const results = await fetch("/api/foodServicePG");
+  */
+  const results = await fetch('/api/foodServicePG');
   const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
   /*
@@ -109,7 +134,7 @@ async function mainEvent() {
         An alternate notation would be "bracket notation" - arrayFromJson["data"]
         Dot notation is preferred in JS unless you have a good reason to use brackets
         The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
-      */
+  */
   // console.table(arrayFromJson.data);
 
   // in your browser console, try expanding this object to see what fields are available to work with
@@ -124,23 +149,24 @@ async function mainEvent() {
   // This IF statement ensures we can't do anything if we don't have information yet
   if (arrayFromJson.data?.length > 0) {
     // the question mark in this means "if this is set at all"
-    submit.style.display = "block"; // let's turn the submit button back on by setting it to display as a block when we have data available
+    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
     // lets hide the load button for now
-    loadAnimation.classList.remove("lds-ellipsis");
-    loadAnimation.classList.add("lds-ellipsis_hidden");
+    loadAnimation.classList.remove('lds-ellipsis');
+    loadAnimation.classList.add('lds-ellipsis_hidden');
 
     let currentList = [];
 
-    form.addEventListener("input", (event) => {
+    form.addEventListener('input', (event) => {
       console.log(event.target.value);
       const filteredList = filterList(currentList, event.target.value);
       injectHTML(filteredList);
+      markerPlace(filteredList, pageMap);
     });
 
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-    form.addEventListener("submit", (submitEvent) => {
+    form.addEventListener('submit', (submitEvent) => {
       // This is needed to stop our page from changing to a new URL even though it heard a GET request
       submitEvent.preventDefault();
 
@@ -149,6 +175,7 @@ async function mainEvent() {
 
       // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(currentList);
+      markerPlace(currentList, pageMap);
 
       // By separating the functions, we open the possibility of regenerating the list
       // without having to retrieve fresh data every time
@@ -162,4 +189,4 @@ async function mainEvent() {
       It's calling the 'mainEvent' function at line 57
       It runs first because the listener is set to when your HTML content has loaded
     */
-document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
